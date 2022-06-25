aa10a67ad19b004530b6bda223d85cc76151fde9
Julio Pagnussat <juliopagnussat@MacBook-Pro-de-Julio.local>
2022-06-25 18:39:49 -0300

    nova alteracao

---
+++ pages/cadimo.js
@@ -0,0 +1,18 @@
+function cadimo () {
+    return (
+        <div>
+            <Head>
+                <title>Julio Cezar Pagnussat</title>
+                <meta name="description" content="Corretor de Imóveis em Chapecó - SC" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <form>
+                    <label>
+                        <input type="text" name="proprietario" />
+                    </label>
+                </form>
+            </main>
+        </div>
+    )
+}
